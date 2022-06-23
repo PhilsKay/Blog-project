b05e9ae0adf5e88f9c06d66e27f445d568113cf2
PhilsKay <philipinhokayode@gmail.com>
2022-06-23 02:10:02 +0100

    added Identity

---
+++ philskayBlog/philskayBlog/Areas/Admin/Controllers/StartController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using philskayBlog.Models;
+using System.Diagnostics;
+
+namespace philskayBlog.Controllers
+{
+    [Area("Admin")]
+    public class StartController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public StartController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
