159e5394144fa6faa59a4b92af146ea0914a140d
PhilsKay <philipinhokayode@gmail.com>
2022-06-26 21:55:43 +0100

    blog

---
+++ Blog/Blog/Data/Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+    public class Context : Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityDbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)   
+        {
+
+        }
+    }
+}
